bc1b4d7198bba9e36a904488d3e56ba2881de11c
unknown <dekos@>
2022-05-30 10:49:18 +0700

    update2

--- diagnosa.dart
+++ diagnosa.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:developer';
 
+//teslagi
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
